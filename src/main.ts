bcb21cf69a636ab4d3f7a7c7c6e0f09144500a33
Willian-Rodrigues <willian.rsilva1994@gmail.com>
2022-10-28 19:45:19 -0300

    feat(create-project): projeto criado com NEST e adição das principais libs

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    origin: ['http://localhost:3000'],
+    allowedHeaders: [
+      'X-Requested-With',
+      'X-HTTP-Method-Override',
+      'Content-Type',
+      'Accept',
+      'Observe',
+      'X-Authorization',
+      'X-Token-Auth',
+      'Authorization',
+    ],
+    methods: 'GET, POST, PUT, DELETE, UPDATE',
+  });
+
+  await app.listen(3000);
+}
+bootstrap();
